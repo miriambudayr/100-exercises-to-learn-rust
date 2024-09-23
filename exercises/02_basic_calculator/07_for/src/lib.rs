12740503abf6cfabe4dd03b271ae87586e59a2ba
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2024-09-23 16:22:26 -0400

    complete 2.7: for-loops

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut product = 1;
+    for num in 1..=(n) {
+        println!("PRODUCT: {:?}", product);
+        product *= num
+    }
+
+    product
 }
 
 #[cfg(test)]
@@ -25,5 +31,10 @@ mod tests {
     #[test]
     fn fifth() {
         assert_eq!(factorial(5), 120);
+    }
+
+    #[test]
+    fn sixth() {
+        assert_eq!(factorial(0), 1)
     }
 }
