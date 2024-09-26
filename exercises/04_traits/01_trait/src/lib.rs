4f26e4b268e48f91f645c6c3f2ebaba00f60deb4
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2024-09-26 13:55:29 -0400

    complete 4.1: trait

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -1,3 +1,21 @@
+use std::u32;
+
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        &self % 2 == 0
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        &self % 2 == 0
+    }
+}
+
 // Define a trait named `IsEven` that has a method `is_even` that returns a `true` if `self` is
 // even, otherwise `false`.
 //
